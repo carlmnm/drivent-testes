394374c36c535265076b5f28296d28a270f1d98a
carlmnm <carlossmiguell@gmail.com>
2023-04-24 13:29:23 -0300

    feat: get hotels route

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
+import { getHotels } from '@/controllers/hotels-controller';
 
 const hotelsRouter = Router();
 
-hotelsRouter.all('/*', authenticateToken);
+hotelsRouter.all('/*', authenticateToken).get('/', getHotels);
 
 export { hotelsRouter };
