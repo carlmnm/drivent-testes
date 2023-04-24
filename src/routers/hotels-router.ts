ad9e33018c113b05e8b6ac2a670ff7f3fa01580e
carlmnm <carlossmiguell@gmail.com>
2023-04-24 16:44:06 -0300

    feat: hotels/:hotelId route

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -4,9 +4,6 @@ import { getHotels, getHotelsWithRooms } from '@/controllers/hotels-controller';
 
 const hotelsRouter = Router();
 
-hotelsRouter
-  //.all('/*', authenticateToken)
-  .get('/', getHotels)
-  .get('/:hotelId', getHotelsWithRooms);
+hotelsRouter.all('/*', authenticateToken).get('/', getHotels).get('/:hotelId', getHotelsWithRooms);
 
 export { hotelsRouter };
